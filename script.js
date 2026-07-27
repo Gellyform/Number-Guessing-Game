0f2cccaa26d1a5c22d681f9a66f8b90ba67b7436
Gellyform <153263199+Gellyform@users.noreply.github.com>
2026-07-27 22:31:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -11,5 +11,42 @@ button.onclick = function(){
   if(playing == true){
     random = Math.floor(Math.random() * 100) + 1;
     boxanswer.style.display = "block";
+    playbtn.textContent = "Guess";
+    readme.textContent = "Guess a number from 1 to 100";
+    playbtn.textContent = "Guess";
+    guess = 0;
+
+    playing = false;
   }
+  else{
+    const answer = Number(textbox.value);
+    if(isNan(answer){
+      description.textContent = "Please only enter a number";
+    }
+    else if(answer > 100 || answer < 1){
+      description.textContent = "Please enter a number from 1 to 100 only";
+    }
+    else if(answer < random ){
+      guess++;
+      description.textContent = "Too Low!";
+  }
+    else if(answer > random ){
+      guess++;
+      description.textContent = "Too High!";
+  }
+    else {
+      description.textContent = "🎊Congratulation you guess the correct number!!🎊";
+      randomnummber.textContent = random;
+      textbox.style.diplay = "none";
+      button.textContent = "Play Again";
+
+      playing = true;
+    }
+    if(guess === maxguess){
+      desciption.textContent = "You've reach the maximum of 10 guesses! The number is";
+      randomnumber.textContent = random;
+      button.textContent = "Play Again";
+      boxanswer.style.display = "none";
+
+      playing = true;
 }
