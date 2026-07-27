7df4485a12de7eafd51d487a0c892a6d4360c7dc
Gellyform <153263199+Gellyform@users.noreply.github.com>
2026-07-27 22:17:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+let description = document.getElementById("readme");
+let randomnumber = document.getElementById("number");
+let textbox = document.getElementById("boxanswer");
+const button = document.getElementById("playbtn");
+let playing = true;
+let geuss;
+const max-guess = 10;
+let random;
 
+button.onclick = function(){
+  if(playing == true){
+    random = Math.floor(Math.random() * 100) + 1;
+    boxanswer.style.display = "block";
+  }
+}
